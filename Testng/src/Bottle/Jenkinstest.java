603efd73566620f6e72cd5701affc0dd8c80d7b5
Aslam0987 <syedaslam457@gmail.com>
2017-08-06 10:51:54 +0530

    latest

---
+++ Testng/src/Bottle/Jenkinstest.java
@@ -0,0 +1,17 @@
+package Bottle;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Jenkinstest {
+	
+	@Test
+	public void test()
+	{
+		
+		System.out.println("test");
+		
+		//Assert.assertEquals(10,11);
+	}
+
+}
